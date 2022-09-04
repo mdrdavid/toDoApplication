b13099922265b24270b01554ac9bb3b6ef7ce1a2
david <davidmatovu88@gmail.com>
2022-09-04 13:06:07 +0300

    connect to mongodb database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
 const app = express()
+
+dotenv.config()
+const connect = ()=>{
+    mongoose.connect("mongodb+srv://admin:admin@onlineshop.x3pfm.mongodb.net/todo?retryWrites=true&w=majority").then(()=>{
+        console.log("Connected to MongoDB")
+    })
+    .catch((err) =>{
+        throw err;
+    });
+}
 const PORT = 5000
 
 app.get("/", (req, res)=>{
     res.send("hello world")
 })
-app.listen(PORT, ()=>console.log(`Server Running at Port ${PORT}`))
+app.listen(process.env.PORT, ()=>{
+connect()
+console.log(`Server Running at Port ${PORT}`)})
