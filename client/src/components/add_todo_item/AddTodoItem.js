6b4e39c8c80adaab4ae91fd7cb37ba34987d5d3f
david <davidmatovu88@gmail.com>
2022-09-06 10:48:03 +0300

    add useState hook to manage input

--- client/src/components/add_todo_item/AddTodoItem.js
+++ client/src/components/add_todo_item/AddTodoItem.js
@@ -1,11 +1,29 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import "./addtodoitem.css"
 
 const  AddTodoItem = () => {
+    const [input, setInput] = useState("")
+
+    const handleChange=(e)=>{
+        setInput(e.target.value)
+        // setInput((prev)=>({...prev, [e.target.name]: e.target.value}))
+    }
+    
+    const handleSubmit= (e)=>{
+        e.preventDefault();
+        setInput("")
+    }
+
   return (
-    <form className="add-t0d0-item">
-        <input type="text" name="item" placeholder="add item" className="add-item"/>
+    <form className="add-todo-item" onSubmit={handleSubmit}>
+        <input type="text" 
+        name="item" 
+        value={input}
+        placeholder="add item" 
+        className="add-item"
+        onChange={handleChange}
+        />
         <button type="submit" className='submit-button'>Add Item</button>
     </form>
   )
